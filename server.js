ab6ae1eaea73c18a8accca81d6f647fd200439e1
xXDerpyXx <bottledcanofdoom@gmail.com>
2017-01-23 21:42:54 -0500

    memes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 console.log("BOOTED");
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var userTotal = 0;
 console.log("GOT EXPRESS");
 var app = express();
@@ -12,17 +13,19 @@ var allMessage = ""
 console.log("GOT SOCKET+HTTP");
 app.use(express.static('.'));
 console.log("STATIC");
+app.use(fileUpload());
+
 io.on('connection', function(socket){
 		userTotal += 1;
   		console.log('a user connected');
   		console.log('user count: '+userTotal)
   		var color = ""
- 		io.emit('return message',allMessage,userTotal);
+ 		io.emit('return message',allMessage,userTotal,false);
  		socket.on('disconnect', function(){
    			console.log('user disconnected');
    			userTotal -= 1;
    			console.log('user count: '+userTotal)
-   			io.emit('return message',allMessage,userTotal);
+   			io.emit('return message',allMessage,userTotal,false);
  	});
 });
 
@@ -42,7 +45,7 @@ socket.on('chat message', function(msg,color){
 		}else{
 			if(msg.length>1){
 				allMessage = '<div style="background-color:#'+color+';padding:3px">'+msg+"</div>" +allMessage;
-				io.emit('return message',allMessage,userTotal);
+				io.emit('return message',allMessage,userTotal,true);
 				console.log('   sending back: "'+msg+'", color: '+color);	
 			}else{
 				console.log('TOO SHORT')
@@ -52,6 +55,21 @@ socket.on('chat message', function(msg,color){
 	
 	console.log('---------------')
 	});
+});
+
+app.post('/upload',function(req,res){
+	var tempFile;
+	console.log("PICTURE UPLOADING 1");
+	tempFile = req.files.tempFile;
+	console.log("PICTURE UPLOADING 2");
+	tempFile.mv('images/picture.png',function(err){
+			if(err){
+				console.log("FILE UPLOAD FAILED");
+			}else{
+				console.log("FILE UPLOADED");
+			}
+	});
+	console.log("PICTURE UPLOADING 3");
 });
 
 console.log("FINISHED");
